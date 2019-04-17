110fb035c68b0d7b0d4ad852c57761d62f4b296c
@Annapetrova19 <ptrv@ukr.net>
2019-04-17 21:35:53 +0300

    second

--- scripts.js
+++ scripts.js
@@ -36,29 +36,44 @@
 //    step 2
 
 
-const list = [2, 4, 6, 7, 9, 0, 10];
+// const list = [2, 4, 6, 7, 9, 0, 10];
 
-function cutArray(arr, from, to) {
-if (isNaN(from) || isNaN(to)){
-  return 'error!';
-}
-  if (arguments.length === 3) {
-    const newArray = [];
-    let counter = 0;
-    for (let i = from; i <= to; i++) {
-      newArray[counter] = arr[i];
-      counter++;
-    }
-    return newArray;
-  } else if (arguments.length === 2) {
-    return arr[from];
-  } else {
-    return 'error!';
+// function cutArray(arr, from, to) {
+// if (isNaN(from) || isNaN(to)){
+//   return 'error!';
+// }
+//   if (arguments.length === 3) {
+//     const newArray = [];
+//     let counter = 0;
+//     for (let i = from; i <= to; i++) {
+//       newArray[counter] = arr[i];
+//       counter++;
+//     }
+//     return newArray;
+//   } else if (arguments.length === 2) {
+//     return arr[from];
+//   } else {
+//     return 'error!';
+//   }
+
+// }
+
+// console.log(list);
+// console.log(cutArray(list, 1, 3));
+// console.log(cutArray(list, 1));
+// console.log(cutArray(list));
+
+
+
+
+
+const recSum = function(from, to){
+  if(to === from) {
+    return to;
+    
   }
-
+  return recSum(from, to - 1) + to;
 }
 
-console.log(list);
-console.log(cutArray(list, 1, 3));
-console.log(cutArray(list, 1));
-console.log(cutArray(list));
+console.log((recSum(2,6)));
+
